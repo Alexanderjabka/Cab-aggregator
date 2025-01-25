e202cbbb67abb73d1ae05171301c4f9e49e8a969
Alexander Popruzhenko <popruzhenko.shura@mail.ru>
2025-01-26 00:03:01 +0300

    create a project

---
+++ src/test/java/org/internship/task/cabaggregator/CabAggregatorApplicationTests.java
@@ -0,0 +1,13 @@
+package org.internship.task.cabaggregator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CabAggregatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
