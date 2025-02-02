7c4ccfe48dfcb820a3946ab25c893b54e9eec665
Alexander Popruzhenko <popruzhenko.shura@mail.ru>
2025-02-02 14:10:32 +0300

    refactor code after review by mentor

--- passenger-service/src/main/java/org/internship/task/passengerservice/util/constantMessages/ExceptionMessages.java
+++ passenger-service/src/main/java/org/internship/task/passengerservice/util/constantMessages/ExceptionMessages.java
@@ -5,5 +5,6 @@ public final class ExceptionMessages {
     public static final String PASSENGER_NOT_FOUND_BY_EMAIL = "Passenger not found with Email: ";
     public static final String PASSENGER_ALREADY_EXISTS = "A Passenger with the same Email already exists: ";
     public static final String PASSENGER_IS_DELETED = "Passenger is deleted and cannot be updated: ";
-    private ExceptionMessages(){}
+    private ExceptionMessages(){
+    }
 }
--- passenger-service/src/main/java/org/internship/task/passengerservice/util/reqEx/PassengerReqEx.java
+++ passenger-service/src/main/java/org/internship/task/passengerservice/util/reqEx/PassengerReqEx.java
@@ -2,5 +2,6 @@ package org.internship.task.passengerservice.util.reqEx;
 
 public final class PassengerReqEx {
     public final static String PHONE_NUMBER = "^\\+375[\\- ]?\\(?\\d{2}\\)?[\\- ]?\\d{7,9}$";
-    private PassengerReqEx(){}
+    private PassengerReqEx(){
+    }
 }
