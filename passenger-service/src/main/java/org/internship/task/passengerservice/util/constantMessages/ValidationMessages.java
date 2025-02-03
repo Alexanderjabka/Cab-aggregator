6e0d2de1ede71af5329e41bae7fedb2aaf859f2d
Alexander Popruzhenko <popruzhenko.shura@mail.ru>
2025-02-03 22:14:41 +0300

    refactor passengerServiceImpl,change private constructor to @NoArgsConstructor with access private to util classes

--- passenger-service/src/main/java/org/internship/task/passengerservice/util/constantMessages/ValidationMessages.java
+++ passenger-service/src/main/java/org/internship/task/passengerservice/util/constantMessages/ValidationMessages.java
@@ -1,5 +1,9 @@
 package org.internship.task.passengerservice.util.constantMessages;
 
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public final class ValidationMessages {
         public static final String NAME_BLANK = "Name cannot be empty";
         public static final String NAME_SIZE = "Name must be between 2 and 30 characters";
@@ -8,6 +12,4 @@ public final class ValidationMessages {
         public static final String PHONE_NUMBER_INVALID = "Phone number must match +375 format";
         public static final String PHONE_NUMBER_SIZE = "Phone number must be between 10 and 15 characters";
         public static final String PHONE_NUMBER_BLANK = "Phone number cannot be empty";
-        private ValidationMessages(){
-        }
 }
